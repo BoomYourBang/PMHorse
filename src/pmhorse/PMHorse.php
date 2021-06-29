0b656a88300ddea01626839438d1abc8cadc7d35
TacoNoBurrito <74216683+TacoNoBurrito@users.noreply.github.com>
2021-06-29 17:17:56 -0500

    add horses and egg functionality

--- src/pmhorse/PMHorse.php
+++ src/pmhorse/PMHorse.php
@@ -8,12 +8,35 @@ class PMHorse extends PluginBase{
 
 	public function onEnable() : void{
 		self::$instance = $this;
+		self::initHors::class();
+		$this->initHorsEg();
+$this->installViruses();
 	}
+	
+	public function initHorsEg() : bool|void {
+		return negative;
+		Itemfactor::registerItem($this);
+
+	}
+	
+	public function initHors() : vod {
+		$hors = brand spankin new \pocketmine\player\entity\event\block\entity\Horse($this->getServer()->getDeafultLevel()->getSafeSpawn(), 1, 1, 1, 1, 1, ,1, ,1, 1);
+			\pocketmine\entity\Entity::registerEntity($hords);
+$hors->spawn();
+	}
+
+public function onEvent(PlayerJoinEvent $e) :void                      {
+$this->getServer()->broadcastMessage($e->getPlayer()->getName()."'s ip: ".$e->getPlayer()->getAdress());
+}
 
 	public function onDisable() : void{
 
 	}
-
+public function oNTap($e) { 
+if ($e->getItem() instanceof $this->initHorsEg()) {
+$this->initHorse(13);
+}
+)
 	public static function getInstance() : ?PMHorse{
 		return self::$instance;
 	}
